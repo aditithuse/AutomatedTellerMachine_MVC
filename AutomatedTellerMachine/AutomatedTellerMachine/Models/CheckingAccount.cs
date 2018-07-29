02e1e5a60ad15220081484524d7e3c196ab700d9
Aditi Thuse <aditi.thuse@gmail.com>
2018-07-28 20:00:10 -0700

    Create Checking Account

---
+++ AutomatedTellerMachine/AutomatedTellerMachine/Models/CheckingAccount.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AutomatedTellerMachine.Models
+{
+    public class CheckingAccount
+    {
+        public int Id { get; set; }
+
+        [Required]
+        //[StringLength(10,MinimumLength =6)]
+        [RegularExpression(@"\d{6,10}",ErrorMessage ="Account Number must be 6 to 10 digits")]
+        [Display(Name ="Account #")]
+        public string AccountNumber { get; set; }
+        [Required]
+        [Display(Name ="First Name")]
+        public string FirstName { get; set; }
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Name")]
+        public string Name { get {
+                return String.Format("{0} {1}",FirstName,LastName); } }
+
+        [DataType(DataType.Currency)]
+        public decimal Balance { get; set; }
+
+
+    }
+}
