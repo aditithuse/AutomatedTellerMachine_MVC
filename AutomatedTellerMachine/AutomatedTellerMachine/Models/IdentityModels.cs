b8c2159aadfccd478602178b1eaa71eed9c71c23
Aditi Thuse <aditi.thuse@gmail.com>
2018-07-28 22:50:50 -0700

    IdentitySystem

--- AutomatedTellerMachine/AutomatedTellerMachine/Models/IdentityModels.cs
+++ AutomatedTellerMachine/AutomatedTellerMachine/Models/IdentityModels.cs
@@ -16,6 +16,8 @@ namespace AutomatedTellerMachine.Models
             // Add custom user claims here
             return userIdentity;
         }
+
+        public string pin { get; set; }
     }
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
